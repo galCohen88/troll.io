b25b5d12ce4ebc8a6e0f5dd0687c366dcfe05459
galcohen <gal.cohen@autodesk.com>
2020-12-29 10:06:13 +0200

    youtube

--- trollio/src/components/popup.jsx
+++ trollio/src/components/popup.jsx
@@ -1,39 +1,44 @@
 import { Howl } from 'howler'
 const electron = window.require("electron")
 const remote = electron.remote;
-// const BrowserWindow = remote.BrowserWindow;
+const BrowserWindow = remote.BrowserWindow;
 
 export function popUp(){
   console.log("popup!")
-  const currentWindow = remote.getCurrentWindow();
-  currentWindow.moveTop();
-  currentWindow.focus();
-  currentWindow.hide();
-  currentWindow.show();
-  currentWindow.setPosition(100,5, true);
-  currentWindow.setPosition(5,100, true);
-  currentWindow.setPosition(100,5, true);
-  currentWindow.setPosition(5,100, true);
-  currentWindow.setPosition(100,5, true);
-  currentWindow.setPosition(5,100, true);
-  currentWindow.setPosition(100,5, true);
-  currentWindow.setPosition(5,100, true);
-  return
+  // const currentWindow = remote.getCurrentWindow();
+  // currentWindow.moveTop();
+  // currentWindow.focus();
+  // currentWindow.hide();
+  // currentWindow.show();
+  // currentWindow.setPosition(100,5, true);
+  // currentWindow.setPosition(5,100, true);
+  // currentWindow.setPosition(100,5, true);
+  // currentWindow.setPosition(5,100, true);
+  // currentWindow.setPosition(100,5, true);
+  // currentWindow.setPosition(5,100, true);
+  // currentWindow.setPosition(100,5, true);
+  // currentWindow.setPosition(5,100, true);
+  // return
+  
+  const win = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+  win.loadURL(`https://youtu.be/jW7fi-9MRUQ?t=38`);
 
-  // const win = new BrowserWindow({
-  //   height: 600,
-  //   width: 800
-  // });
-  // win.loadURL(`https://google.com`);
-  // win
-
-  // var music = new Howl({
-  //   src: ["/Users/galcohen/dev/aaa/troll.io/trollio/src/media/audio/guitar.mp3"],
-  //   html5: false,
-  //   loop: true,
-  //   autoplay: true,
-  //   preload: true
-  // })
-  // music.play();
+  
+  win.webContents.once('dom-ready', () => {
+    var maximum =  1000;
+    var minimum =  0;
+    var moves = 20;
+    var count =  0;
+    while (count<moves) {
+      count += 1;
+      var x = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+      var y = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+      win.setPosition(x,y, true); 
+    }
+  
+  });
 
 }

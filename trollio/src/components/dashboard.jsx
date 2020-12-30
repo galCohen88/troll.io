35b9f9eed86774259f454bf8dc21ff3fa3720e78
galcohen <gal.cohen@autodesk.com>
2020-12-30 17:55:47 +0200

    ..

--- trollio/src/components/dashboard.jsx
+++ trollio/src/components/dashboard.jsx
@@ -20,6 +20,10 @@ export function Dashboard(props) {
     resizeWindow(945, 896);
     const [show, setShow] = useState(false);
     const [message, setMessage] = useState(null);
+    const [messageM, setModalMessage] = useState(false);
+
+    const handleModalClose = () => { setShow(false); }
+    const handleModalShow = () => { setShow(true); }
 
     const handleClose = () => { setMessage(null); setShow(false); resizeWindow(600, 500);}
 
@@ -120,7 +124,12 @@ export function Dashboard(props) {
                 <label>TROLL.IO</label>
                 </div>
                 <div>
-                    <Button onClick={}></Button>
+                <Button variant="primary" onClick={handleModalShow}>
+                Send troll modal
+                </Button>
+                <Modal show={show} onHide={handleModalClose}>
+                    <SendTrollModal onCancel={handleModalClose}></SendTrollModal>
+                </Modal>
                 </div>
                 <div>
                     {modal}
@@ -143,3 +152,73 @@ export function Dashboard(props) {
     
 }
 
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import ReactAudioPlayer from 'react-audio-player';
+// import ReactDOM from "react-dom";
+// import { Button, Modal } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./styles.css";
+// const electron = window.require("electron")
+// const remote = electron.remote;
+// const BrowserWindow = remote.BrowserWindow;
+// const nativeImage = remote.nativeImage;
+// const  Notification = remote.Notification;
+
+// export function ReceiverModal() {
+ 
+//     const [show, setShow] = useState(false);
+//     const appIcon = nativeImage.createFromPath('logo.png')
+//     const notification = {
+//         title: 'Success!',
+//         body: 'User Gal Cohen got trolled',
+//         icon: appIcon
+//       }
+
+//     const handleClose = () => { setShow(false); resizeWindow(600, 500); }
+//     const handleShow = () => { setShow(true); resizeWindow(945, 896); new Notification(notification).show();}
+//     // TODO remove, change to variables from web socket
+//     var img = "http://interactive.nydailynews.com/2016/05/simpsons-quiz/img/simp1.jpg";
+//     var audio = 'http://dnfw.org/hl/sound/misc/doh.wav'
+//     var modalText = "Doohhhhhoooo!!!!!!"
+//     var sender = 'Gal Cohen'
+//     var autoPlay = true
+
+      
+//     return (
+//         <div className="App">
+//             <Button variant="primary" onClick={handleShow}>
+//                 Receiver modal
+//           </Button>
+
+//             <Modal show={show} onHide={handleClose} className="ReceiverModal">
+
+
+//                 <Modal.Body>
+//                 <Modal.Title>
+//                         <img className='Center' src={img}></img>
+//                     </Modal.Title>
+//                 <ReactAudioPlayer src={audio} autoPlay={autoPlay} />
+//                     <div className='ModalText'>{modalText}</div>
+
+                    
+//                     <div className='Sender'> Sent with 
+//                     <img className='Heart' src='https://galcosagemaker.s3.amazonaws.com/heart.png'></img>
+//                     by <label className='SenderName'> {sender} </label>
+//                     </div>
+
+//                 </Modal.Body>
+//             </Modal>
+//         </div>
+//     );
+// }
+
+// function resizeWindow(x, y) {
+//     const currentWindow = remote.getCurrentWindow();
+//     currentWindow.setSize(x, y)
+// }

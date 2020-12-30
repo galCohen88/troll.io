51a4a1f3140d3a920975f2ddf54e163f48620de2
Arad Bar Sadeh <arad.bar.sadeh@autodesk.com>
2020-12-30 13:47:10 +0200

    Complete score table in dashboard

--- trollio/src/components/dashboard.jsx
+++ trollio/src/components/dashboard.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import './dashboard.css'
 
 
 class Dashboard extends React.Component {
-    renderScoreRow(score) {
-        return <tr>
-            <td>{score.user}</td>
-            <td>{score.sent}</td>
-            <td>{score.received}</td>
+    constructor(props) {
+        super(props);
+        this.renderScoreRow = this.renderScoreRow.bind(this);
+    }
+
+    renderScoreRow(score, index) {
+        let trClass = 'not-top-three';
+        if (0 <= index && index <= 2) {
+            trClass = 'top-three'
+        }
+        if (score.email === this.props.currentUser) {
+            trClass += ' current-user';
+        }
+
+        let name = score.name;
+        if (index === 0) {
+            name += ' (1st)';
+        } else if (index === 1) {
+            name += ' (2nd)';
+        } else if (index === 2) {
+            name += ' (3rd)';
+        }
+
+        const image = require(`../users/${score.email}.jpeg`).default;
+
+        return <tr className={trClass}>
+            <td className="photo">
+                <img className="user-image" src={image} alt={score.name} />
+            </td>
+            <td className="name">{name}</td>
+            <td className="sent">{score.sent}</td>
+            <td className="received">{score.received}</td>
         </tr>
     }
 
@@ -15,12 +43,17 @@ class Dashboard extends React.Component {
 
         return (
             <table id='scores'>
-                <tr>
-                    <th> </th>
-                    <th>Sent</th>
-                    <th>Received</th>
-                </tr>
-                {rows}
+                <thead>
+                    <tr>
+                        <th> </th>
+                        <th> </th>
+                        <th>Sent</th>
+                        <th>Received</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
             </table>
         );
     }

39d08c65dab31a0c5f783f6454c1bd16f025d5dd
galcohen <gal.cohen@autodesk.com>
2020-12-30 10:10:39 +0200

    ..

--- trollio/public/electron.js
+++ trollio/public/electron.js
@@ -1,29 +1,33 @@
 const { app, BrowserWindow, TouchBar } = require('electron')
 const { TouchBarLabel, TouchBarButton } = TouchBar
+const {nativeImage} = require('electron');
 const path = require("path");
 const isDev = require("electron-is-dev");
 let mainWindow;
 
-const troll = new TouchBarButton({
-    label: '😈 Troll',
-    backgroundColor: '#7FFFD4',
-    click: () => {}
-  })
-  
-  const music = new TouchBarButton({
-    label: '🎸 Audio',
-    backgroundColor: '#008B8B',
-    click: () => {}
-  })
+let happyTroll = new TouchBarButton({
+  'backgroundColor': '#000000',
+  'icon': nativeImage.createFromPath(path.join(__dirname, '..', 'src', 'rotate-old.png')).resize({
+    width: 50,
+    height: 50,
+  }),
+  'iconPosition': 'center',
+});
 
-  const play = new TouchBarButton({
-    label: '🎮 Play',
-    backgroundColor: '#DC143C',
-    click: () => {}
-  })
+let greeenTroll = new TouchBarButton({
+  'backgroundColor': '#000000',
+  'icon': nativeImage.createFromPath(path.join(__dirname, '..', 'src', 'rotate.png')).resize({
+    width: 50,
+    height: 50,
+  }),
+  'iconPosition': 'right',
+});
+
+const label = new TouchBarLabel({label: "TROOOOOLIIINNNNN'", 'textColor': '#4DEC9F'})
+
 
   const touchBar = new TouchBar({
-    items: [troll, music, play]
+    items: [happyTroll, label, greeenTroll]
   })
 
   
@@ -38,7 +42,7 @@ function createWindow() {
     });
     mainWindow.loadURL(isDev ? "http://localhost:3000" :`file://${path.join(__dirname, "../build/index.html")}`);
     mainWindow.on("closed", () => (mainWindow = null));
-    // mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
     mainWindow.setTouchBar(touchBar)
     mainWindow.show();
 }

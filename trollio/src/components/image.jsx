d20bc0ff15a00d778c2a271089adc6d9944cb3e7
galcohen <gal.cohen@autodesk.com>
2020-12-30 18:08:52 +0200

    move image

--- trollio/src/components/image.jsx
+++ trollio/src/components/image.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 import ReactAudioPlayer from 'react-audio-player';
-// const electron = window.require("electron")
-// const remote = electron.remote;
-// const BrowserWindow = remote.BrowserWindow;
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
+const electron = window.require("electron")
+const remote = electron.remote;
 
 export function Image(props) {
+    if (props.includeMotion){
+        const currentWindow = remote.getCurrentWindow();
+        setTimeout(() => {moveWindowOnceReady(currentWindow);}, 500);
+        
+    }
+    
     return (
         <Modal.Body>
         <Modal.Title>
@@ -25,4 +29,22 @@ export function Image(props) {
     );
 }
  
-  
+function moveWindowOnceReady(win){
+    win.moveTop();
+    win.focus();
+    win.hide();
+    win.show();
+    var x_max = 1000
+    var x_min = 0
+    var y_max = 100
+    var y_mix = 0
+    var moves = 20;
+    var count =  0;
+    while (count<moves) {
+      count += 1;
+      var x = Math.floor(Math.random() * (x_max - x_min + 1)) + x_min;
+      var y = Math.floor(Math.random() * (y_max - y_mix + 1)) + y_mix;
+      win.setPosition(x,y, true); 
+    }
+    win.maximize()
+}
